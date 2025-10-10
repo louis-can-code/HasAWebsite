c2b9054a1c1ad5a5e43b224061ebaa2d5f044613
Glob13 <louis_hudson@btinternet.com>
2025-10-10 15:33:35 +0100

    Cleaned up and fixed matching against get_user_by_session_token/1 output

--- lib/has_a_website_web/plugs/ensure_role.ex
+++ lib/has_a_website_web/plugs/ensure_role.ex
@@ -24,27 +24,30 @@ defmodule HasAWebsiteWeb.Plugs.EnsureRole do
   @doc false
   @spec call(Plug.Conn.t(), atom() | [atom()]) :: Plug.Conn.t()
   def call(conn, roles) do
+    roles = List.wrap(roles)
     user_token = get_session(conn, :user_token)
 
     (user_token &&
-      Accounts.get_user_by_session_token(user_token))
+       Accounts.get_user_by_session_token(user_token))
     |> has_role?(roles)
     |> maybe_halt(conn)
   end
 
-  defp has_role?(%User{} = user, roles) when is_list(roles),
-    do: Enum.any?(roles, &has_role?(user, &1))
+  defp has_role?({%User{} = user, _token_inserted_at}, roles) when is_list(roles) do
+    Enum.any?(roles, &has_role?(user, &1))
+  end
 
   defp has_role?(%User{role: role}, role), do: true
+
   defp has_role?(_user_or_false, _role), do: false
 
   defp maybe_halt(true, conn), do: conn
+
   defp maybe_halt(_, conn) do
     conn
-    |> Controller.put_flash(:error, "Unauthorised")
-    |> Controller.redirect(to: signed_in_path(conn))
+    |> put_status(:forbidden)
+    |> Controller.put_view(HasAWebsiteWeb.ErrorHTML)
+    |> Controller.render(:"403")
     |> halt()
   end
-
-  defp signed_in_path(_conn), do: "/"
 end
