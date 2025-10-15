bdd6cd9891db048d88a7540cb38b55fb213c5291
Glob13 <louis_hudson@btinternet.com>
2025-10-15 18:08:32 +0100

    comment fixture

--- test/support/fixtures/blog_fixtures.ex
+++ test/support/fixtures/blog_fixtures.ex
@@ -18,4 +18,25 @@ defmodule HasAWebsite.BlogFixtures do
     {:ok, post} = HasAWebsite.Blog.create_post(scope, attrs)
     post
   end
+
+  @doc """
+  Generate a comment.
+  """
+  @spec comment_fixture(
+          HasAWebsite.Accounts.Scope.t(),
+          HasAWebsite.Blog.Post.t(),
+          attrs :: map(),
+          replying_to :: HasAWebsite.Blog.Comment.t() | nil
+        ) ::
+          HasAWebsite.Blog.Comment.t()
+  def comment_fixture(scope, post, attrs \\ %{}, replying_to \\ nil) do
+    attrs =
+      Enum.into(attrs, %{
+        content: "some content"
+      })
+
+    {:ok, comment} = HasAWebsite.Blog.create_comment(scope, post, attrs, replying_to)
+
+    comment
+  end
 end
