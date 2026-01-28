4133eae0c81a7a624013ce6c822688e9da34876a
louis-can-code <louis_hudson@btinternet.com>
2026-01-28 16:26:33 +0000

    Formatted

--- mix.exs
+++ mix.exs
@@ -68,7 +68,7 @@ defmodule HasAWebsite.MixProject do
       {:dns_cluster, "~> 0.2.0"},
       {:bandit, "~> 1.5"},
       {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
-      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
     ]
   end
 
